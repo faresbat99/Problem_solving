cbc74c2167115c8f754afe884ce147f4c20e4d90
faresbat99 <faresbat99@gmail.com>
2023-03-19 04:33:32 +0200

    adding more affective solution in flowers problem

--- leetcode/Beginner level/605.Can Place Flowers.php
+++ leetcode/Beginner level/605.Can Place Flowers.php
@@ -7,17 +7,25 @@
  */
 function canPlaceFlowers(array $flowerbed, int $n)
 {
-    $flowerbed[] = 0;
-    array_unshift($flowerbed, 0);
-    for ($i = 1; $i < count($flowerbed) - 1; $i++) {
-        if ($flowerbed[$i] == 0 and $flowerbed[$i + 1] == 0 and $flowerbed[$i - 1] == 0) {
-            $n--;
+    // $flowerbed[] = 0;
+    // array_unshift($flowerbed, 0);
+    // for ($i = 1; $i < count($flowerbed) - 1; $i++) {
+    //     if ($flowerbed[$i] == 0 and $flowerbed[$i + 1] == 0 and $flowerbed[$i - 1] == 0) {
+    //         $n--;
+    //         $flowerbed[$i] = 1;
+    //     }
+    //     if ($n <= 0) {
+    //         return true;
+    //     }
+    // }
+    // return false;
+    $k = 0;
+    for ($i = 0; $i < count($flowerbed) && $k < $n; $i++) {
+        if ($flowerbed[$i] == 0 && $flowerbed[$i - 1] !== 1 && $flowerbed[$i + 1] !== 1) {
+            $k++;
             $flowerbed[$i] = 1;
         }
-        if ($n <= 0) {
-            return true;
-        }
     }
-    return false;
+    return $k == $n;
 }
-var_dump(canPlaceFlowers([1, 0, 1, 0, 0],1));
+var_dump(canPlaceFlowers([1, 0, 1, 0, 0], 1));
